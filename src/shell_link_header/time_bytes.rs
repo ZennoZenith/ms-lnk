5c233525143dc3740add4d8b524814f20fe05148
ZennoZenith <ZennoZenith@proton.me>
2024-06-18 10:42:49 +0530

    refa: changed Filetime struct format

--- src/shell_link_header/time_bytes.rs
+++ src/shell_link_header/time_bytes.rs
@@ -22,25 +22,19 @@ use std::time::SystemTime;
 use crate::shared::{ldap_timestamp_to_unix, unix_to_ldap_timestamp};
 
 #[derive(Debug, PartialEq)]
-pub struct Filetime {
-    ldap_timestamp: u64,
-}
+pub struct Filetime(u64);
 
 impl Default for Filetime {
     fn default() -> Self {
         match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
             Ok(n) => {
                 println!("n.seconds: {}", n.as_secs());
-                Self {
-                    ldap_timestamp: unix_to_ldap_timestamp(n.as_secs() as i64, 0),
-                }
+                Self(unix_to_ldap_timestamp(n.as_secs() as i64, 0))
             }
             // SystemTime before UNIX EPOCH!
             Err(_) => {
                 println!("n.seconds: ERROR");
-                Self {
-                    ldap_timestamp: unix_to_ldap_timestamp(0, 0),
-                }
+                Self(unix_to_ldap_timestamp(0, 0))
             }
         }
     }
@@ -48,13 +42,11 @@ impl Default for Filetime {
 
 impl Filetime {
     pub fn new_from_ldap(ldap_timestamp: u64) -> Self {
-        Self { ldap_timestamp }
+        Self(ldap_timestamp)
     }
 
     pub fn new_from_unix(unix_seconds: i64, unix_nanoseconds: u64) -> Self {
-        Self {
-            ldap_timestamp: unix_to_ldap_timestamp(unix_seconds, unix_nanoseconds),
-        }
+        Self(unix_to_ldap_timestamp(unix_seconds, unix_nanoseconds))
     }
 
     // OPTIMIZE
@@ -68,12 +60,12 @@ impl Filetime {
             ldap_timestamp += *x as u64;
         }
         println!();
-        Self { ldap_timestamp }
+        Self(ldap_timestamp)
     }
 
     pub fn to_bytes(&self) -> [u8; 8] {
         let mut time_bytes = vec![];
-        let mut ldap_timestamp = self.ldap_timestamp;
+        let mut ldap_timestamp = self.0;
         for _ in 0..8 {
             time_bytes.push((ldap_timestamp & 0b1111_1111) as u8);
             ldap_timestamp >>= 8;
@@ -82,11 +74,11 @@ impl Filetime {
     }
 
     pub fn get_unix_time(&self) -> (i64, u64) {
-        ldap_timestamp_to_unix(self.ldap_timestamp)
+        ldap_timestamp_to_unix(self.0)
     }
 
     pub fn get_ldap_time(&self) -> u64 {
-        self.ldap_timestamp
+        self.0
     }
 }
 
